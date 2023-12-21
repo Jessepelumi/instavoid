18167800f7cbcd2440dddb79951e44514a8b3591
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-12-21 02:26:18 +0100

    auth state provider done

---
+++ lib/state/auth/providers/auth_state_provider.dart
@@ -0,0 +1,8 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instavoid/state/auth/models/auth_state.dart';
+import 'package:instavoid/state/auth/notifiers/auth_state_notifier.dart';
+
+//AuthStateNotifier - Notifier, AuthState - Notifier object
+final authStateProvider = StateNotifierProvider<AuthStateNotifier, AuthState>(
+  (_) => new AuthStateNotifier(),
+);
