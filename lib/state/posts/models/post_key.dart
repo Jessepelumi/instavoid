18eed17edad4a07cc9248af7f192b8653ea25707
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-02 22:49:11 +0100

    created post settings

---
+++ lib/state/posts/models/post_key.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class PostKey {
+  static const userId = "";
+  static const message = "";
+  static const createdAt = "";
+  static const thumbnailUrl = "";
+  static const fileUrl = "";
+  static const fileType = "";
+  static const fileName = "";
+  static const aspectRatio = "";
+  static const postSettings = "";
+  static const thumbnailStorageId = "";
+  static const originalFileStorageId = "";
+}
