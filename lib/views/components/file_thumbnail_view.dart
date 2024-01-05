3ce7e72579d010124f2fe77e9cc21f057f223856
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-05 10:57:54 +0100

    done with the thumbnail view

--- lib/views/components/file_thumbnail_view.dart
+++ lib/views/components/file_thumbnail_view.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instavoid/state/image_upload/models/thumbnail_request.dart';
+import 'package:instavoid/state/image_upload/providers/thumbnail_provider.dart';
+import 'package:instavoid/views/components/animations/loading_animation_view.dart';
+import 'package:instavoid/views/components/animations/small_error_animation_view.dart';
 
 class FileThumbnailView extends ConsumerWidget {
-  const FileThumbnailView({super.key});
+  final ThumbnailRequest thumbnailRequest;
+
+  const FileThumbnailView({
+    super.key,
+    required this.thumbnailRequest,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final thumbnail = ref.watch(
+      thumbnailProvider(thumbnailRequest),
+    );
+
+    return thumbnail.when(
+      data: (ImageWithAspectRatio) {
+        return AspectRatio(
+          aspectRatio: ImageWithAspectRatio.aspectRatio,
+          child: ImageWithAspectRatio.image,
+        );
+      },
+      error: (error, stackTrace) {
+        return const SmallErrorAnimationView();
+      },
+      loading: () {
+        return const LoadingAnimationView();
+      },
+    );
   }
 }
