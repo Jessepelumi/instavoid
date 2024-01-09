3bbcbbab8abe4a8784fb88a46abd691575fdf866
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-09 12:38:38 +0100

    added isUploading to is_loading_provider

--- lib/state/providers/is_loading_provider.dart
+++ lib/state/providers/is_loading_provider.dart
@@ -1,8 +1,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instavoid/state/auth/providers/auth_state_provider.dart';
+import 'package:instavoid/state/image_upload/providers/image_upload_provider.dart';
 
 final isLoadingProvider = Provider<bool>((ref) {
   final authState = ref.watch(authStateProvider);
+  final isUploadingImage = ref.watch(imageUploadProvider);
 
-  return authState.isLoading;
+  return authState.isLoading || isUploadingImage;
 });
