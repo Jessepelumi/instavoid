75cca96d39cc5f2105c288e1cfa42cc14d3fcf02
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-10 18:12:21 +0100

    done with functionalities for sending, posting and sorting comments

---
+++ lib/state/comments/extensions/comment_sorting_by_request.dart
@@ -0,0 +1,22 @@
+import 'package:instavoid/enums/date_sorting.dart';
+import 'package:instavoid/state/comments/models/comments.dart';
+import 'package:instavoid/state/comments/models/post_comments_request.dart';
+
+extension Sorting on Iterable<Comment> {
+  Iterable<Comment> applySortingFrom(RequestForPostAndComments request) {
+    if (request.sortByCreatedAt) {
+      final sortedDocuments = toList()
+        ..sort((a, b) {
+          switch (request.dateSorting) {
+            case DateSorting.newestOnTop:
+              return b.createdAt.compareTo(a.createdAt);
+            case DateSorting.oldestOnTop:
+              return a.createdAt.compareTo(b.createdAt);
+          }
+        });
+      return sortedDocuments;
+    } else {
+      return this;
+    }
+  }
+}
---
+++ lib/state/comments/models/post_comments_request.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:instavoid/enums/date_sorting.dart';
+import 'package:instavoid/state/posts/typedefs/post_id.dart';
+
+@immutable
+class RequestForPostAndComments {
+  final PostId postId;
+  final bool sortByCreatedAt;
+  final DateSorting dateSorting;
+  final int? limit;
+
+  const RequestForPostAndComments({
+    required this.postId,
+    required this.sortByCreatedAt,
+    required this.dateSorting,
+    required this.limit,
+  });
+
+  @override
+  bool operator ==(covariant RequestForPostAndComments other) =>
+      postId == other.postId &&
+      sortByCreatedAt == other.sortByCreatedAt &&
+      dateSorting == other.dateSorting &&
+      limit == other.limit;
+
+  @override
+  int get hashCode => Object.hashAll([
+        postId,
+        sortByCreatedAt,
+        dateSorting,
+        limit,
+      ]);
+}
