02620c21f8e6aa9c59e5fb1712e741ae8021daa9
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-12 12:59:35 +0100

    implemented post and video view for post details page

---
+++ lib/views/components/post/post_image_or_video_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:instavoid/state/image_upload/models/file_type.dart';
+import 'package:instavoid/state/posts/models/post.dart';
+import 'package:instavoid/views/components/post/post_image_view.dart';
+import 'package:instavoid/views/components/post/post_video_view.dart';
+
+class PostImageOrVideoView extends StatelessWidget {
+  final Post post;
+
+  const PostImageOrVideoView({
+    super.key,
+    required this.post,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    switch (post.fileType) {
+      case FileType.image:
+        return PostImageView(post: post);
+      case FileType.video:
+        return PostVideoView(post: post);
+      default:
+        return const SizedBox();
+    }
+  }
+}
