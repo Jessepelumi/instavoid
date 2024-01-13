1f69ae7f7fe7b689ab8fed42e40eb907a22e7995
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-13 07:37:21 +0100

    done with the like button

---
+++ lib/views/components/like_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instavoid/state/auth/providers/user_id_provider.dart';
+import 'package:instavoid/state/likes/models/like_dislike_request.dart';
+import 'package:instavoid/state/likes/providers/has_liked_post_provider.dart';
+import 'package:instavoid/state/likes/providers/like_dislike_post_provider.dart';
+import 'package:instavoid/state/posts/typedefs/post_id.dart';
+import 'package:instavoid/views/components/animations/small_error_animation_view.dart';
+
+class LikeButton extends ConsumerWidget {
+  final PostId postId;
+
+  const LikeButton({
+    super.key,
+    required this.postId,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final hasLiked = ref.watch(
+      hasLikedPostProvider(postId),
+    );
+
+    return hasLiked.when(
+      data: (hasLiked) {
+        return IconButton(
+          icon: FaIcon(
+            hasLiked ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart,
+          ),
+          onPressed: () {
+            final userId = ref.read(userIdProvider);
+            if (userId == null) {
+              return;
+            }
+
+            final likeRequest = LikeDislikeRequests(
+              postId: postId,
+              likedBy: userId,
+            );
+
+            ref.read(
+              likeDislikePostProvider(likeRequest),
+            );
+          },
+        );
+      },
+      error: (error, stackTrace) {
+        return const SmallErrorAnimationView();
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
