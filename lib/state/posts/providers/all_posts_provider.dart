a1750b7fd031f8e227b8f53811a72deec37f94d5
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-16 16:01:03 +0100

    implemented functionality to view all posts in the database

---
+++ lib/state/posts/providers/all_posts_provider.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instavoid/state/constants/firebase_collection_name.dart';
+import 'package:instavoid/state/constants/firebase_field_name.dart';
+import 'package:instavoid/state/posts/models/post.dart';
+
+final allPostsProvider = StreamProvider.autoDispose<Iterable<Post>>((ref) {
+  final controller = StreamController<Iterable<Post>>();
+
+  final subscription = FirebaseFirestore.instance
+      .collection(FirebaseCollectionName.posts)
+      .orderBy(FirebaseFieldName.createdAt, descending: true)
+      .snapshots()
+      .listen((snapshot) {
+    final posts = snapshot.docs.map(
+      (doc) => Post(
+        postId: doc.id,
+        json: doc.data(),
+      ),
+    );
+    controller.sink.add(posts);
+  });
+
+  ref.onDispose(() {
+    subscription.cancel();
+    controller.close();
+  });
+
+  return controller.stream;
+});
