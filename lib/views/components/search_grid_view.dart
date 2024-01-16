e088c0755860424e8845ffa7b8ab3fbfe0774df4
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-01-16 15:02:05 +0100

    done with search functionality

---
+++ lib/views/components/search_grid_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instavoid/state/posts/providers/posts_by_search_term_provider.dart';
+import 'package:instavoid/state/posts/typedefs/search_term.dart';
+import 'package:instavoid/views/components/animations/data_not_found_animation_view.dart';
+import 'package:instavoid/views/components/animations/empty_contents_with_text_animation_view.dart';
+import 'package:instavoid/views/components/animations/error_animation_view.dart';
+import 'package:instavoid/views/components/post/posts_grid_view.dart';
+import 'package:instavoid/views/constants/strings.dart';
+
+class SearchGridView extends ConsumerWidget {
+  final SearchTerm searchTerm;
+
+  const SearchGridView({
+    super.key,
+    required this.searchTerm,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (searchTerm.isEmpty) {
+      return const EmptyContentsWithTextAnimationView(
+          text: Strings.enterYourSearchTerm);
+    }
+
+    final posts = ref.watch(
+      postBySearchTermProvider(searchTerm),
+    );
+
+    return posts.when(
+      data: (posts) {
+        if (posts.isEmpty) {
+          return const DataNotFoundAnimationView();
+        } else {
+          return PostGridView(posts: posts);
+        }
+      },
+      error: (error, stackTrace) {
+        return const ErrorAnimationView();
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
